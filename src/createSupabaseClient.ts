46f1a2e0dbecbc83e8e23909433ad90fa1d28222
Ulrich Stark <github@ustark.de>
2025-10-28 14:26:53 +0100

    make it even simpler

--- src/createSupabaseClient.ts
+++ src/createSupabaseClient.ts
@@ -1,18 +1,5 @@
-import { createServerClient } from "@supabase/ssr";
-import { SupabaseClient } from "@supabase/supabase-js";
-import { getCookies, setCookie } from "@tanstack/react-start/server";
+import { getCookies } from "@tanstack/react-start/server";
 
-export function createSupabaseClient(): SupabaseClient {
-    return createServerClient("", "", {
-        cookies: {
-            getAll() {
-                return Object.entries(getCookies()).map(([name, value]) => ({ name, value }));
-            },
-            setAll(cookies) {
-                cookies.forEach((cookie) => {
-                    setCookie(cookie.name, cookie.value);
-                });
-            },
-        },
-    });
+export function createSupabaseClient() {
+    getCookies();
 }
