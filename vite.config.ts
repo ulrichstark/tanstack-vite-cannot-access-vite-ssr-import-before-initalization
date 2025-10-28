08d80bbc0456d5d82a4f38a1025042d20768fe96
Ulrich Stark <github@ustark.de>
2025-10-28 14:33:13 +0100

    even simpler 3

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
 
-export default defineConfig(({}) => {
-    return {
-        plugins: [tanstackStart(), nitroV2Plugin(), react()],
-        server: { port: 3000, open: true, host: true },
-    };
-});
+export default {
+    plugins: [tanstackStart(), nitroV2Plugin(), react()],
+    server: { open: true },
+};
