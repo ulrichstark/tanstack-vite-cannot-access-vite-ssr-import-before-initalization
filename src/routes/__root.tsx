46884c165acfc582959c3d36ece0512c95ea6cab
Ulrich Stark <github@ustark.de>
2026-01-04 20:19:20 +0100

    add index route

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,8 @@
 import { createRootRoute } from "@tanstack/react-router";
+import { ReactNode } from "react";
 
 export const Route = createRootRoute({ shellComponent: ShellComponent });
 
-function ShellComponent() {
-    return null;
+function ShellComponent({ children }: { children: ReactNode }) {
+    return children;
 }
